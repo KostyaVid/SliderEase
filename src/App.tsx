e4ff984a9227565eaf726f09d4a1468fdcee9b96
KostyaVid <constat@live.com>
2022-08-11 13:05:56 +0300

    prepare template

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [size, setSize] = useState([1000, 500]);
+  return (
+    <div className="App">
+      <header>
+        <h1>SWIPER-EASE</h1>
+      </header>
+      <main>
+        <div className="form">
+          <label className="input">
+            Width container: {size[0]}px
+            <input
+              type="range"
+              min={50}
+              max={1500}
+              step={1}
+              onChange={(e) => {
+                setSize([Number(e.target.value), size[1]]);
+              }}
+              value={size[0]}
+            />
+          </label>
+          <label className="input">
+            Hight container: {size[1]}px
+            <input
+              type="range"
+              min={50}
+              max={1000}
+              step={1}
+              onChange={(e) => {
+                setSize([size[0], Number(e.target.value)]);
+              }}
+              value={size[1]}
+            />
+          </label>
+        </div>
+        <div
+          className="container__swiper"
+          style={{
+            width: `${size[0]}px`,
+            height: `${size[1]}px`,
+          }}></div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
